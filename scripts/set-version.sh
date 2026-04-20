829995c3b7685caa479d6b1831c435d6e899e008
Siak Hooi <siakhooi@gmail.com>
2026-04-20 07:20:49 +0000

    fix typo

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -12,6 +12,6 @@ sed -i 'src/RPMS/siakhooi-buildo.spec' -e 's@Name:           .*@Name:           
 sed -i 'publish-to-apt.yml' -e 's@siakhooi-buildo_[0-9.]*_amd64.deb@siakhooi-buildo_'"$COMPONENT_VERSION"'_amd64.deb@g'
 sed -i 'publish-to-apt.yml' -e 's@docs/pool/main/binary-amd64/siakhooi-buildo_[0-9.]*_amd64.deb@docs/pool/main/binary-amd64/siakhooi-buildo_'"$COMPONENT_VERSION"'_amd64.deb@g'
 
-# Update publish-to-rpm.yml
-sed -i 'publish-to-rpm.yml' -e 's@siakhooi-buildo-[0-9.]*-1.fc43.noarch.rpm@siakhooi-buildo-'"$COMPONENT_VERSION"'-1.fc43.noarch.rpm@g'
-sed -i 'publish-to-rpm.yml' -e 's@docs/siakhooi-buildo-[0-9.]*-1.fc43.noarch.rpm@docs/siakhooi-buildo-'"$COMPONENT_VERSION"'-1.fc43.noarch.rpm@g'
+# Update publish-to-rpms.yml
+sed -i 'publish-to-rpms.yml' -e 's@siakhooi-buildo-[0-9.]*-1.fc43.noarch.rpm@siakhooi-buildo-'"$COMPONENT_VERSION"'-1.fc43.noarch.rpm@g'
+sed -i 'publish-to-rpms.yml' -e 's@docs/siakhooi-buildo-[0-9.]*-1.fc43.noarch.rpm@docs/siakhooi-buildo-'"$COMPONENT_VERSION"'-1.fc43.noarch.rpm@g'
